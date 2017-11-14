dddd3712eebc1442ee057fa4f69cc7cb3beb0a98
Lucas Luitjes <lucas@luitjes.it>
2017-11-14 17:59:49 +0100

    Convert certificate to json-serializable format

--- lib/certcollector.ex
+++ lib/certcollector.ex
@@ -7,4 +7,20 @@ defmodule Certcollector do
          :ok <- :ssl.close(socket),
          do: :public_key.pkix_decode_cert(cert, :otp)
   end
+
+  def to_json_safe(cert) when is_tuple(cert) do
+    Tuple.to_list(cert) |> to_json_safe
+  end
+
+  def to_json_safe(cert) when is_list(cert) do
+    Enum.map(cert, &to_json_safe/1)
+  end
+
+  def to_json_safe(cert) when is_binary(cert) do
+    if String.valid?(cert) do cert else Base.encode64(cert) end
+  end
+
+  def to_json_safe(cert) do
+    cert
+  end
 end
--- test/certcollector_test.exs
+++ test/certcollector_test.exs
@@ -1,8 +1,12 @@
 defmodule CertcollectorTest do
   use ExUnit.Case
 
-  test "greets the world" do
+  test "grab a certificate from a server" do
   	assert Certcollector.get_cert("google.com") == google_fixture()
+  end
+
+  test "test json encode" do
+  	google_fixture() |> Certcollector.to_json_safe |> Poison.encode
   end
 
   defp google_fixture do
