11634b3ac60405367f244990e47c5c9c3a3f5a0e
Lucas Luitjes <lucas@luitjes.it>
2017-11-14 15:42:20 +0100

    Grab ssl certificate and parse it

--- lib/certcollector.ex
+++ lib/certcollector.ex
@@ -1,18 +1,10 @@
 defmodule Certcollector do
-  @moduledoc """
-  Documentation for Certcollector.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Certcollector.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def get_cert(host) do
+    with charlist <- to_charlist(host),
+         :ok <- :ssl.start,
+         {:ok, socket} <- :ssl.connect(charlist, 443, [], 10000),
+         {:ok, cert} <- :ssl.peercert(socket),
+         :ok <- :ssl.close(socket),
+         do: :public_key.pkix_decode_cert(cert, :otp)
   end
 end
--- test/certcollector_test.exs
+++ test/certcollector_test.exs
@@ -1,8 +1,13 @@
 defmodule CertcollectorTest do
   use ExUnit.Case
-  doctest Certcollector
 
   test "greets the world" do
-    assert Certcollector.hello() == :world
+  	assert Certcollector.get_cert("google.com") == google_fixture()
   end
+
+  defp google_fixture do
+  	with {:ok, pem} <- File.read("test/fixtures/googlecom.crt"),
+  	     [{:Certificate, pem_decoded, :not_encrypted}] <- :public_key.pem_decode(pem),
+  	     do: :public_key.pkix_decode_cert(pem_decoded, :otp)
+	end
 end
